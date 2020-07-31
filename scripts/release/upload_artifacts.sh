09a3bd26e7ba3b417c7252f47c91684228b4abea
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-07-31 17:51:47 -0300

    Improve release script

--- scripts/release/upload_artifacts.sh
+++ scripts/release/upload_artifacts.sh
@@ -5,22 +5,21 @@
 #
 # https://docs.gitlab.com/ce/api/projects.html#upload-a-file
 
+# Use your own ID here
 PROJECT_ID=853
 
 # Download latest artifacts to your local machine:
-curl --output artifacts.zip --header "PRIVATE-TOKEN: $IGG_GITLAB_TOKEN" \
-  "https://gitlab.igg.uni-bonn.de/api/v4/projects/$PROJECT_ID/jobs/artifacts/master/download?job=build"
+curl --silent \
+  --header "PRIVATE-TOKEN: $IGG_GITLAB_TOKEN" \
+  --output artifacts.zip \
+  "https://gitlab.igg.uni-bonn.de/api/v4/projects/$PROJECT_ID/jobs/artifacts/master/download?job=deploy"
 
 # Upload this artifacts, check for the output!!!
-curl --header "PRIVATE-TOKEN: $IGG_GITLAB_TOKEN" \
+echo "PLEASE MAKE SURE YOU COPY THIS LINE INTO THE release_data.json FILE:"
+echo "========================================================================"
+curl --silent \
+  --header "PRIVATE-TOKEN: $IGG_GITLAB_TOKEN" \
   --form "file=@artifacts.zip" \
-  "https://gitlab.igg.uni-bonn.de/api/v4/projects/$PROJECT_ID/uploads"
-
-echo "\e[1m\e[93m===========================================================\
-==========================================================================\e[0m"
-echo "\e[1m\e[93mPLEASE MAKE SURE YOU COPY THE HASH NUMBER FROM THE MARKDOWN \
-ENTRY AND UPDATE IT IN THE release_data.json FILE !! ...\e[0m"
-echo "\e[1m\e[93mThis number should be something similar to \
-\e[1m\e[36me550dfc73e150a3e43e6e530ff30212a ...\e[0m"
-echo "\e[1m\e[93m===========================================================\
-==========================================================================\e[0m"
+  "https://gitlab.igg.uni-bonn.de/api/v4/projects/$PROJECT_ID/uploads" |
+  jq '.'markdown
+echo "========================================================================"
