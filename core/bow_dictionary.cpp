3f39c0f69d19a531b0cf134151b3892ce5806a31
Ekansh Sharma <ekanshh.sharma@gmail.com>
2024-03-03 20:00:27 +0100

    Add my naive kmeans implementation

--- core/bow_dictionary.cpp
+++ core/bow_dictionary.cpp
@@ -2,25 +2,107 @@
 
 #include <algorithm>
 #include <iostream>
-#include <opencv2/features2d.hpp>
+#include <numeric>
+#include <random>
 #include <vector>
 
 namespace ipb {
 
 cv::Mat kMeans(const std::vector<cv::Mat> &descriptors, int k, int max_iter) {
-    cv::Mat descriptors_mat;
+    auto total_descriptors =
+            std::accumulate(descriptors.begin(), descriptors.end(), 0,
+                            [](int sum, const cv::Mat &desc) { return sum + desc.rows; });
+    std::cout << "Total descriptors: " << total_descriptors << std::endl;
+    int descriptor_dim = descriptors[0].cols;
+    cv::Mat concatenated_descriptors(total_descriptors, descriptor_dim, CV_32F);
+
+    int offset = 0;
     for (const auto &desc : descriptors) {
-        cv::Mat desc_float;
-        desc.convertTo(desc_float, CV_32F);
-        descriptors_mat.push_back(desc_float);
+        desc.copyTo(concatenated_descriptors.rowRange(offset, offset + desc.rows));
+        offset += desc.rows;
     }
 
     cv::Mat centers;
-    cv::Mat labels;
+    std::vector<int> labels;
     cv::TermCriteria criteria(cv::TermCriteria::EPS, max_iter, 1e-4);
-    cv::kmeans(descriptors_mat, k, labels, criteria, 1, cv::KMEANS_RANDOM_CENTERS, centers);
+    cv::kmeans(concatenated_descriptors, k, labels, criteria, 1, cv::KMEANS_RANDOM_CENTERS,
+               centers);
 
     return centers;
+}
+
+// my implementation of kMeans
+cv::Mat naive_kMeans(const std::vector<cv::Mat> &descriptors, int k, int max_iter) {
+    // Accumulate descriptors into a single matrix
+    int total_descriptors =
+            std::accumulate(descriptors.begin(), descriptors.end(), 0,
+                            [](int sum, const cv::Mat &desc) { return sum + desc.rows; });
+    std::cout << "Total descriptors: " << total_descriptors << std::endl;
+    int descriptor_dim = descriptors[0].cols;
+    cv::Mat concatenated_descriptors(total_descriptors, descriptor_dim, CV_32F);
+    int offset = 0;
+    for (const auto &desc : descriptors) {
+        desc.copyTo(concatenated_descriptors.rowRange(offset, offset + desc.rows));
+        offset += desc.rows;
+    }
+    std::cout << "Concatenated descriptors: " << concatenated_descriptors.size << std::endl;
+
+    // Randomly initialize means
+    static std::random_device seed;
+    static std::mt19937 random_number_generator(seed());
+    std::uniform_int_distribution<int> indices(0, concatenated_descriptors.rows - 1);
+    cv::Mat means(k, descriptor_dim, CV_32F);
+    for (int i = 0; i < k; ++i) {
+        int index = indices(random_number_generator);
+        concatenated_descriptors.row(index).copyTo(means.row(i));
+    }
+    std::cout << "Initial means: " << means.size << "\n" << std::endl;
+
+    // Iterations
+    for (int iter = 0; iter < max_iter; ++iter) {
+        // Assign descriptors to clusters
+        std::vector<std::vector<int>> cluster_indices(k);
+        for (int i = 0; i < total_descriptors; ++i) {
+            cv::Mat distances;
+            cv::batchDistance(concatenated_descriptors.row(i), means, distances, CV_32F,
+                              cv::noArray(), cv::NORM_L2SQR);
+            cv::Point minLoc;
+            cv::minMaxLoc(distances, nullptr, nullptr, &minLoc, nullptr);
+            cluster_indices[minLoc.x].push_back(i);
+        }
+
+        // check if there are no empty clusters, otherwise it will crash
+        for (int i = 0; i < k; ++i) {
+            if (cluster_indices[i].empty()) {
+                std::cerr << "Empty cluster " << i << " detected, reinitializing means"
+                          << std::endl;
+                std::cerr << "Naive clustering failed" << std::endl;
+                std::cerr << "Reinitialze kmeans with less clusters" << std::endl;
+            }
+        }
+
+        // Compute new means
+        cv::Mat new_means(k, descriptor_dim, CV_32F);
+        for (int i = 0; i < k; ++i) {
+            cv::Mat cluster_descriptors(cluster_indices[i].size(), descriptor_dim, CV_32F);
+            for (size_t j = 0; j < cluster_indices[i].size(); ++j) {
+                concatenated_descriptors.row(cluster_indices[i][j])
+                        .copyTo(cluster_descriptors.row(j));
+            }
+            cv::reduce(cluster_descriptors, new_means.row(i), 0, cv::REDUCE_AVG);
+        }
+
+        // Check convergence
+        if (cv::norm(new_means, means) < 1e-8) {
+            std::cout << "Converged after " << iter << " iterations" << std::endl;
+            return new_means;
+        }
+
+        // Update means
+        means = new_means.clone();
+    }
+
+    return means;
 }
 
 BowDictionary &BowDictionary::GetInstance() {
@@ -30,10 +112,11 @@ BowDictionary &BowDictionary::GetInstance() {
 void BowDictionary::build(int max_iterations,
                           std::size_t size,
                           const std::vector<cv::Mat> &descriptors) {
-    auto vocabulary = kMeans(descriptors, static_cast<int>(size), max_iterations);
+    auto vocabulary = naive_kMeans(descriptors, static_cast<int>(size), max_iterations);
     set_vocabulary(vocabulary);
 }
 
+const cv::Mat &BowDictionary::vocabulary() const { return vocabulary_; }
 void BowDictionary::set_vocabulary(const cv::Mat &vocabulary) { vocabulary_ = vocabulary.clone(); }
 
 std::size_t BowDictionary::size() const {
--- core/bow_dictionary.hpp
+++ core/bow_dictionary.hpp
@@ -1,11 +1,10 @@
 #pragma once
 
 #include <opencv2/core.hpp>
+#include <string>
 #include <vector>
 
 namespace ipb {
-
-cv::Mat kMeans(const std::vector<cv::Mat> &descriptors, int k, int max_iter);
 
 class BowDictionary {
 protected:
@@ -15,23 +14,26 @@ public:
     static BowDictionary &GetInstance();
 
     BowDictionary(const BowDictionary &) = delete;
-    BowDictionary(BowDictionary &&) = delete;
     BowDictionary &operator=(const BowDictionary &) = delete;
+    BowDictionary(BowDictionary &&) = delete;
     BowDictionary &operator=(BowDictionary &&) = delete;
     ~BowDictionary() = default;
 
     void build(int max_iterations, std::size_t size, const std::vector<cv::Mat> &descriptors);
-
     void save(const std::string &filename) const;
     void load(const std::string &filename);
 
-    [[nodiscard]] const cv::Mat &vocabulary() const { return vocabulary_; }
+    [[nodiscard]] const cv::Mat &vocabulary() const;
     void set_vocabulary(const cv::Mat &vocabulary);
-
     [[nodiscard]] std::size_t size() const;
     [[nodiscard]] bool empty() const;
 
 private:
-    cv::Mat vocabulary_{};  // codebook
+    cv::Mat vocabulary_{};
 };
+
+cv::Mat kMeans(const std::vector<cv::Mat> &descriptors, int k, int max_iter);
+// my implementation of kMeans
+cv::Mat naive_kMeans(const std::vector<cv::Mat> &descriptors, int k, int max_iter);
+
 }  // namespace ipb
